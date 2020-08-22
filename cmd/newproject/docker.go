7c400bbe24861c0f03dfd74a8abb3aa4b9b8c888
Bert Krätge <bert@kraetge.de>
2020-08-22 18:25:20 +0200

    backup

---
+++ cmd/newproject/docker.go
@@ -0,0 +1,19 @@
+package newproject
+
+//func checkDockerImageExists(imageName string) (bool, error) {
+//cli, err := client.NewEnvClient()
+//if err != nil {
+//	panic(err)
+//}
+//
+//var options types.ImageListOptions
+//options.All = true
+//imagesummary, imageErr := cli.ImageList(context.Background(), options)
+//if imageErr != nil {
+//	panic(nil)
+//}
+//for _, s := range imagesummary {
+//	fmt.Println(s.ID)
+//}
+//return false, err
+//}
